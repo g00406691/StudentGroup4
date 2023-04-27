42cfef346d8c9437fe3f5d3ae604bf1944af6c98
G004091 <126460087+G004091@users.noreply.github.com>
2023-04-28 00:02:30 +0100

    Add files via upload

---
+++ assets/js/popup.js
@@ -0,0 +1,138 @@
+let popup = document.getElementById("popup");
+
+function openPopup(){
+  popup.classList.add("open-popup");
+}
+
+function closePopup(){
+  popup.classList.remove("open-popup");
+}
+
+let popup1 = document.getElementById("popup1");
+
+function openPopup1(){
+  popup1.classList.add("open-popup1");
+}
+
+function closePopup1(){
+  popup1.classList.remove("open-popup1");
+}
+let popup3 = document.getElementById("popup3");
+
+function openPopup3(){
+  popup3.classList.add("open-popup3");
+}
+
+function closePopup3(){
+  popup3.classList.remove("open-popup3");
+}
+let popup4 = document.getElementById("popup4");
+
+function openPopup4(){
+  popup4.classList.add("open-popup4");
+}
+
+function closePopup4(){
+  popup4.classList.remove("open-popup4");
+}
+let popup5 = document.getElementById("popup5");
+
+function openPopup5(){
+  popup5.classList.add("open-popup5");
+}
+
+function closePopup5(){
+  popup5.classList.remove("open-popup5");
+}
+let popup6 = document.getElementById("popup6");
+
+function openPopup6(){
+  popup6.classList.add("open-popup6");
+}
+
+function closePopup6(){
+  popup6.classList.remove("open-popup6");
+}
+let popup7 = document.getElementById("popup7");
+
+function openPopup7(){
+  popup7.classList.add("open-popup7");
+}
+
+function closePopup7(){
+  popup7.classList.remove("open-popup7");
+}
+let popup8 = document.getElementById("popup8");
+
+function openPopup8(){
+  popup8.classList.add("open-popup8");
+}
+
+function closePopup8(){
+  popup8.classList.remove("open-popup8");
+}
+let popup9 = document.getElementById("popup9");
+
+function openPopup9(){
+  popup9.classList.add("open-popup9");
+}
+
+function closePopup9(){
+  popup9.classList.remove("open-popup9");
+}
+let popup10 = document.getElementById("popup10");
+
+function openPopup10(){
+  popup10.classList.add("open-popup10");
+}
+
+function closePopup10(){
+  popup10.classList.remove("open-popup10");
+}
+let popup11 = document.getElementById("popup11");
+
+function openPopup11(){
+  popup11.classList.add("open-popup11");
+}
+
+function closePopup11(){
+  popup11.classList.remove("open-popup11");
+}
+let popup12 = document.getElementById("popup12");
+
+function openPopup12(){
+  popup12.classList.add("open-popup12");
+}
+
+function closePopup12(){
+  popup12.classList.remove("open-popup12");
+}
+let popup13 = document.getElementById("popup13");
+
+function openPopup13(){
+  popup13.classList.add("open-popup13");
+}
+
+function closePopup13(){
+  popup13.classList.remove("open-popup13");
+}
+
+let popup14 = document.getElementById("popup14");
+
+function openPopup14(){
+  popup14.classList.add("open-popup14");
+}
+
+function closePopup14(){
+  popup14.classList.remove("open-popup14");
+}
+
+let popup15 = document.getElementById("popup15");
+
+function openPopup15(){
+  popup15.classList.add("open-popup15");
+}
+
+function closePopup15(){
+  popup15.classList.remove("open-popup15");
+}
